1a037a6fc90bd2e7fcb8a4e0b09dd24c64e9053b
Alessandro Riva <alex27riva@gmail.com>
2023-01-04 13:50:22 +0100

    Working conversion

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Python script to convert .kml file to .json
 
 ## Installation 
-- `pip3 install pykml`
+- `pip3 install -r requirements.txt`
--- convert.py
+++ convert.py
@@ -11,15 +11,41 @@ root = parser.fromstring(kml_bytes)
 # Create an empty list
 location_list = []
 
-# Iterate over the placemarks in the KML data
-for placemark in root.Document.Placemark:
-  location = {
-    'name': placemark.name.text,
-    'description': placemark.description.text if hasattr(placemark, 'description') else None,
-    'latitude': placemark.Point.coordinates.text.split(',')[0],
-    'longitude': placemark.Point.coordinates.text.split(',')[1],
-  }
-  location_list.append(location)
+def process_element(element):
+  """Recursively process an element and its children."""
+  if element.tag.endswith('}Placemark'):
+    hasValidTag = False
+
+    try:
+      hasValidTag = hasattr(element.Point, "coordinates")
+    except:
+      hasValidTag = False
+
+    if hasValidTag:
+      lat = float(element.Point.coordinates.text.split(',')[0])
+      lon = float(element.Point.coordinates.text.split(',')[1])
+      print(lat, lon, type(lat), type(lon))
+      location = {
+        'name': element.name.text,
+        'description': element.description.text if hasattr(element, 'description') else None,
+        'latitude': lat,
+        'longitude': lon,
+      }
+      print(location)
+      location_list.append(location)
+  elif element.tag.endswith('}Folder'):
+     # Skip folders with specific names
+    if element.name.text not in ['Tracks', 'Waypoints', 'Points']:
+      # Process the element's children
+      for child in element.iterchildren():
+        process_element(child)
+  else:
+    # Process the element's children
+    for child in element.iterchildren():
+      process_element(child)
+
+# Process the root element
+process_element(root)
 
 # Create the JSON object
 json_data = {
